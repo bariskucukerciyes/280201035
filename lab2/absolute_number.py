79d2610a43a8cc837025b631b577e6dac9ea32e1
u29224254 <baris.kucukerciyes@gmail.com>
2020-10-26 08:24:17 +0000

    absolute value

---
+++ lab2/absolute_number.py
@@ -0,0 +1,10 @@
+given_number = float(input("Enter Number:"))
+
+if given_number < 0:
+    final_number = given_number*-1
+else:
+    final_number = given_number
+
+print("Absolute value of your number is", final_number)
+
+   
